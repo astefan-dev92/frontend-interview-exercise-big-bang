353cb5ead1cc9b49b06972f64d631ae32a94a9e9
Alex Stefan <alexandra.stefan@thirdspacelearning.com>
2025-02-25 20:37:59 +0000

    Set up libraries: zustand, material-ui, fontawesome

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,8 @@
 import automataLogo from '../assets/automata.png'
 import './App.css'
 
+import { Box, Button } from '@mui/material'
+
 function App() {
   return (
     <>
@@ -11,6 +13,10 @@ function App() {
       </div>
       <h1>Frontend Exercise</h1>
       <h2>Rock, Paper, Scissors, Lizard, Spock</h2>
+
+      <Box>
+        <Button>Play!</Button>
+      </Box>
     </>
   )
 }
